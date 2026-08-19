cb61c063f0d8b834d8b913c59624a6c45b7138c8
MdAamerSk <mdaamersk786@gmail.com>
2026-08-19 23:59:29 +0530

    feat: implement authentication and dashboard layouts with routing

---
+++ src/app/layouts/DashboardLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Outlet } from "react-router";
+
+const DashboardLayout = () => {
+  
+
+  return  (
+  <div>
+    <h1>this is fpr nav</h1>
+    <Outlet />
+    </div>)
+};
+
+export default DashboardLayout;
